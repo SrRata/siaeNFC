2b09a2283baa476687cb1c258b080ec3f30559a7
SrRata <informaticauemffb@gmail.com>
2026-02-02 00:45:11 -0500

    tabla

--- public/dashboard-teacher/views/students.php
+++ public/dashboard-teacher/views/students.php
@@ -37,3 +37,89 @@
     </div>
 </div>
 
+<div class="table">
+    <div class="table-top">
+        <h3 class="table-title">
+            <i data-lucide="graduation-cap"></i>
+            3ro de bachillerato especialidad informatica
+        </h3>
+    </div>
+    <table class="table-body">
+        <thead>
+            <tr>
+                <th class="table-header table-data table-data-first">Foto</th>
+                <th class="table-header table-data">Nombre</th>
+                <th class="table-header table-data">Matricula/ID</th>
+                <th class="table-header table-data">Asistencia hoy</th>
+                <th class="table-header table-data table-data-last">Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td class="table-data table-data-first"></td>
+                <td class="table-data table-data-name">Justin Alvarez</td>
+                <td class="table-data"><div class="badge badge-gray">0156464646</div></td>
+                <td class="table-data">
+                    <div class="badge badge-green"><div class="circle"></div>Presente</div>
+                </td>
+                <td class="table-data table-data-last">
+                    <div class="table-action">
+                        <a href="#" class="link">
+                            Ver historial
+                            <i data-lucide="external-link"></i>
+                        </a>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td class="table-data table-data-first"></td>
+                <td class="table-data table-data-name">Luis Matailo</td>
+                <td class="table-data"><div class="badge badge-gray">0150072668</div></td>
+                <td class="table-data">
+                    <div class="badge badge-yellow"><div class="circle"></div>Atraso</div>
+                </td>
+                <td class="table-data table-data-last">
+                    <div class="table-action">
+                        <a href="#" class="link">
+                            Ver historial
+                            <i data-lucide="external-link"></i>
+                        </a>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td class="table-data table-data-first"></td>
+                <td class="table-data table-data-name">Braulio Guambaña</td>
+                <td class="table-data"><div class="badge badge-gray">0113366645</div></td>
+                <td class="table-data">
+                    <div class="badge badge-red"><div class="circle"></div>Ausente</div>
+                </td>
+                <td class="table-data table-data-last">
+                    <div class="table-action">
+                        <a href="#" class="link">
+                            Ver historial
+                            <i data-lucide="external-link"></i>
+                        </a>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td class="table-data table-data-first"></td>
+                <td class="table-data table-data-name">Bryam Illescas</td>
+                <td class="table-data"><div class="badge badge-gray">0113366645</div></td>
+                <td class="table-data">
+                    <div class="badge badge-green"><div class="circle"></div>Presente</div>
+                </td>
+                <td class="table-data table-data-last">
+                    <div class="table-action">
+                        <a href="#" class="link">
+                            Ver historial
+                            <i data-lucide="external-link"></i>
+                        </a>
+                    </div>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
